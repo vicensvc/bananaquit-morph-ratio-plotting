986b4239dac956df73c3a14b6f45673bca8f9514
Vicens Vila-Coury <73007446+vicensvc@users.noreply.github.com>
2024-04-10 16:58:10 -0400

    Fixing file structure

--- morph_ratio_plotting_template.R
+++ morph_ratio_plotting_template.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(scatterpie)
 library(sf)
 
-grenada_map <- st_read("grenada_full_4326.shp")
+grenada_map <- st_read("base_map/grenada_full_4326.shp")
 morph_ratio_data <- read.csv("morph_ratio_data.csv")
 
 ggplot() + 
@@ -19,4 +19,3 @@ ggplot() +
         axis.line = element_line(colour = "white")) + 
   labs(x = "longitude", y = "latitude") + 
   annotate(geom = "text", x = -61.64, y = 12.00, label = "max = # birds \nmin =  # \nmedian = #", hjust = 0, vjust = 0)
-
